92aeffa3cee1f86671921cfe9eec409b11b1c1be
marwenwill <marwenop@gmail.com>
2018-03-19 13:27:49 +0100

    added back end

---
+++ Front/Angular2_CLI_Full_Project/src/app/layouts/full-layout.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MyServiceService } from '../my-service.service';
+import { Response } from '@angular/http';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './full-layout.component.html'
+})
+export class FullLayoutComponent {
+    
+  public disabled: boolean = false;
+  public status: {isopen: boolean} = {isopen: false};
+
+  public toggled(open: boolean): void {
+    console.log('Dropdown is now: ', open);
+  }
+
+  public toggleDropdown($event: MouseEvent): void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
+  }
+  constructor(private myService:MyServiceService) {
+
+  }
+  public onSubmit(name: string) {
+    this.myService.sendData({ name: name})
+      .subscribe(
+        data => console.log(data)
+      );
+  }
+ 
+}
---
+++ Front/Angular2_CLI_Full_Project/src/app/my-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from "@angular/http";
+import 'rxjs/Rx';
+
+@Injectable()
+export class MyServiceService {
+  url:string;
+  
+	constructor(public http:Http) {
+		this.url = "http://127.0.0.1:8000/scrumboard";
+	}
+
+    public getAll(): any {
+        
+        return this.http.get(this.url+"/lists")
+          .map((response: Response) => response.json());
+    }
+
+    public sendData(user: any){
+      const body = JSON.stringify(user);
+      const headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      return this.http.post(this.url+"/lists/", body, {
+        headers: headers
+      })
+        .map((data: Response) => data.json())
+
+    }
+}
